5a7e4cbd3a47f03d96fcc39cc7305fb42b76ba20
Surendra Maurya <smaurya@dkincglobal.com>
2023-09-08 21:41:29 +0530

    Chnages for UI

--- web-development.php
+++ web-development.php
@@ -2,39 +2,8 @@
 include_once('header-page.php');
   ?>
 
-<!-- <section class="slider_section ">
-  <div class="container ">
-      <div class="row">
-        <div class="card mx-auto">
-                <div class="card-body">
-                    <h2 class="card- text-center  ">What is Web development? </h2>
-                    <p class="card-text  text-muted font-italic text-center">
-                      Web development is the work involved in developing a website for the Internet (World Wide Web) or an intranet (a private network).[1] Web development can range from developing a simple single static page of plain text to complex web applications, electronic businesses, and social network services. A more comprehensive list of tasks to which Web development commonly refers, may include Web engineering, Web design, Web content development, client liaison, client-side/server-side scripting, Web server and network security configuration, and e-commerce development.
-                    <h4 class="card- text-center font-weight-normal ">Process</h4>
-                    <p class="card-text  text-muted text-center">
-                    1 Discovery
-                    2 Information Architecture
-                    3 Wireframes
-                    4 Web Design
-                    5 Content
-                    6 Website Development
-                    </p>
-                    <h4 class="card- text-center font-weight-normal">What is UI Design?</h4>
-                    <p class="card-text  text-muted text-center">
-                      UI Design is the process of making the app look good. It’s during the UI process that the visual presentation of an app is decided. UI designers are responsible for making sure each screen a user encounters has a consistent and appealing look and feel.
-                     </p>
-                    <h4 class="card- text-center font-weight-normal">Best Programming Languages</h4>
-                    <p class="card-text  text-muted text-center">
-                    1 Python is one of the most powerful and best multi-purpose languages used for Web Development, as well as data analysis. It is an open-source language that is extremely easy to learn and use and also easy to deploy. It is considered one of the best languages to learn for Web Development as it is largely used for the development of scalable web pages and applications.
-                    2 PHP is one of the best languages for Web Development. Although it faces good competition from other languages like JavaScript and Python, there is still a huge requirement for professionals who can work with PHP in the market today. Besides, PHP is a dynamic and general-purpose language that is used in the development of server-side applications. It is maintained by an open-source platform that can work across Mac, UNIX, and Windows systems.
-                    3 Java is a versatile OOP language used for Android development, as well as desktop and web application development. It is a platform-independent multi-purpose language used for programming and has been a popular choice of language in various organizations for decades. Most companies have adopted it due to its stability. It is one of the best languages for Web Development in 2022 that you should definitely learn.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-  </section> -->
-  <section class="slider_section">
+
+  <section class="slider_section" style="margin: 5%;">
   <div class="container">
     <div class="row">
       <div class="col-md-8 mx-auto">
